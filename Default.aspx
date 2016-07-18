144488b2f51e0012357ed304d7b7c97a4ddfa488
rapharma <raphael_arantes26@yahoo.com.br>
2016-07-18 14:45:26 -0300

    Editing some comments

--- Default.aspx
+++ Default.aspx
@@ -9,7 +9,7 @@
 <body>
     <form id="form1" runat="server">
     <div>
-
+        <!--Empty Default-->
     </div>
     </form>
 </body>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -16,7 +16,7 @@ namespace ChangeImg
             /// </summary>
             /// Author:     rapharma
 
-            //Date setted according with _begindate.AddDays(2) of the "for"
+            //Date settled according with _begindate.AddDays(2) of the "for"
             //Two days before the DateTime.Now.Date
             DateTime _vbegindate = new DateTime(2016, 07, 11);
 
@@ -29,7 +29,7 @@ namespace ChangeImg
             for (DateTime _begindate = _vbegindate; _begindate.Date < DateTime.Now.Date; _begindate = _begindate.AddDays(2))
             {
                 count += 1;
-                //When limit of images in the folder is reached, it back to first image
+                //When limit of images in the folder is reached, it back to the first image
                 if (count == qtdimages)
                 {
                     count = 1;
